60a228fd87ff737a8247d5e29cde1f27e340e748
ashach18 <ashach18@gmail.com>
2022-02-23 17:04:25 -0500

    git commit hello

--- client/lab_4/script.js
+++ client/lab_4/script.js
@@ -1,25 +1,45 @@
-
 let slidePosition = 0;
 const slides = document.getElementsByClassName('carousel_item');
 const totalSlides= slides.length;
 
 document. 
  getElementById('carousel_button--next')
- .addEventListener("click", function()) {
-     moveToNextSlide();
- }
+ .addEventListener("click", function() {
+        moveToNextSlide();
+  });
 
 
- document. 
+document. 
  getElementById('carousel_button--prev')
- .addEventListener("click", function()) {
+ .addEventListener("click", function() {
      moveToPrevSlide();
- }
+ });
 
+function updateSlidePosition() {
+ for(let slide of slides){
+    slide.classList.remove('carousel_item--visible');
+    slide.classList.add('carousel_item--hidden');
+}
+ slides[slidePosition].classList.add('carousel_item--visible');
+}
+
+ 
  function moveToNextSlide() {
-     console.log('hello next');
+  updateSlidePosition();
+
+if (slidePosition === totalSlides-1) {
+    slidePosition = 0;
+
+}else {
+    slidePosition++;
  }
+}
   
-  function moveToPrevSlide() {
-      console.log('hello prev ');
+function moveToPrevSlide() {
+ if (slidePosition === totalSlides) {
+  slidePosition = 0;
+
+  }else {
+     slidePosition--;
+ }
   }
