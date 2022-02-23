9b5f2c254247b3c1d4740b5f6748343826c49606
ashach18 <ashach18@gmail.com>
2022-02-23 16:37:53 -0500

    git commit "hello"

--- client/lab_4/script.js
+++ client/lab_4/script.js
@@ -1,7 +1,25 @@
+
 let slidePosition = 0;
 const slides = document.getElementsByClassName('carousel_item');
 const totalSlides= slides.length;
 
-console.log(slides);
+document. 
+ getElementById('carousel_button--next')
+ .addEventListener("click", function()) {
+     moveToNextSlide();
+ }
 
 
+ document. 
+ getElementById('carousel_button--prev')
+ .addEventListener("click", function()) {
+     moveToPrevSlide();
+ }
+
+ function moveToNextSlide() {
+     console.log('hello next');
+ }
+  
+  function moveToPrevSlide() {
+      console.log('hello prev ');
+  }
